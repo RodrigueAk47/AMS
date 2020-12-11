0d26506b4ad2a2241c37580d7faba0d2f44b559f
rodrigue <rodrigueassohou@gmail.com>
2020-12-11 18:41:28 +0000

    melange

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AcceuilComponent } from './acceuil/acceuil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AcceuilComponent,}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
