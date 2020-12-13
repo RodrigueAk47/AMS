abb354607a181c21535aceb1452c1536b77476c8
rodrigue <rodrigueassohou@gmail.com>
2020-12-13 12:04:28 +0000

    amelioration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
 import { AcceuilComponent } from './acceuil/acceuil.component';
 import { ParametreComponent } from './parametre/parametre.component';
 
@@ -11,7 +11,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes,{preloadingStrategy:PreloadAllModules})],
+    RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
