12821adbaad9d3288151b35151d989a8c9fff912
rodrigue <rodrigueassohou@gmail.com>
2020-12-17 19:23:22 +0000

    mise a jour des routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
+import { RegisterComponent } from './authentification/register/register.component';
 import { AccueilComponent } from './client/accueil/accueil.component';
+import { ParametreComponent } from './client/parametre/parametre.component';
+import { TelechargementComponent } from './client/telechargement/telechargement.component';
 
 
 
 const routes: Routes = [
   {path: '', component: AccueilComponent},
+  {path: 'parametre', component: ParametreComponent},
+  {path: 'telechargement', component: TelechargementComponent},
+  {path: 'inscription', component: RegisterComponent},
 ];
 
 @NgModule({
