b744816c44bfb86ec2c1546cab6c1cb05c962d10
rodrigue <rodrigueassohou@gmail.com>
2020-12-18 12:05:13 +0000

    perfection

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
 import { RegisterComponent } from './authentification/register/register.component';
 import { AccueilComponent } from './client/accueil/accueil.component';
+import { ContactComponent } from './client/contact/contact.component';
 import { ParametreComponent } from './client/parametre/parametre.component';
 import { TelechargementComponent } from './client/telechargement/telechargement.component';
 
@@ -11,6 +12,8 @@ const routes: Routes = [
   {path: '', component: AccueilComponent},
   {path: 'parametre', component: ParametreComponent},
   {path: 'telechargement', component: TelechargementComponent},
+  {path: 'inscription', component: RegisterComponent},
+  {path: 'a-propos', component: ContactComponent},
   {path: 'inscription', component: RegisterComponent},
 ];
 
