232a9a2e01cb7498fbf17e4c7cd7e05db3b3ae46
rodrigue <rodrigueassohou@gmail.com>
2021-01-15 11:54:37 +0000

    completer la formation sage comptabilite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,6 @@ import * as firebase from 'firebase'
 })
 export class AppComponent {
   constructor() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyDH5BrfJ3XexfWTqyLkHENeGSEsSncupyM",
-      authDomain: "ams-stefy.firebaseapp.com",
-      databaseURL: "https://ams-stefy-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "ams-stefy",
-      storageBucket: "ams-stefy.appspot.com",
-      messagingSenderId: "638067250330",
-      appId: "1:638067250330:web:0f3ffa1cd7c0a44e427b23",
-      measurementId: "G-MG6VWBMN6B"
-      };
-      firebase.initializeApp(firebaseConfig);
-  }
-}
 
+}
+}
